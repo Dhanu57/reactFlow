4cea2d5cc6c24191801b3f364bbdcc9c035cf4ef
Dhanu57 <dhanashree5796@gmail.com>
2024-03-12 15:44:51 +0530

    edit node name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
 import ReactFlow, {
   ReactFlowProvider,
   addEdge,
   useNodesState,
   useEdgesState,
   Controls,
-  applyNodeChanges
+  applyNodeChanges,
+  updateNode,
+  useUpdateNodeInternals
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 
@@ -16,16 +18,17 @@ import './TextUpdater.css'
 import TextUpdaterNode from './TextUpdaterNode';
 
 const initialNodes = [
-  {
-    id: '1',
-    type: 'input',
-    data: { label: 'input node' },
-    position: { x: 250, y: 5 },
-  }
+  // {
+  //   id: '1',
+  //   type: 'input',
+  //   data: { label: 'input node' },
+  //   position: { x: 250, y: 5 },
+  // }
 ];
 
 let id = 0;
 const getId = () => `dndnode_${id++}`;
+const defaultViewport = { x: 0, y: 0, zoom: 1.5 };
 
 const DnDFlow = () => {
   const reactFlowWrapper = useRef(null);
@@ -34,6 +37,36 @@ const DnDFlow = () => {
   const [reactFlowInstance, setReactFlowInstance] = useState(null);
   const [showJson, setShowJson] = useState(true);
   const [json, setJson] = useState()
+
+  const [nodeName, setNodeName] = useState('');
+  
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === '1') {
+          // it's important that you create a new object here
+          // in order to notify react flow about the change
+          node.data = {
+            ...node.data,
+            label: nodeName,
+          };
+        }
+
+        return node;
+      })
+    );
+  }, [nodeName, setNodes]);
+
+  useEffect(() => {
+     nodes.map(ele => {
+      if(ele.selected){
+        ele.data = {
+          ...ele.data,
+          label: nodeName,
+        };
+      }
+    })
+  }, [nodeName, setNodes]);
 
   const onNodesChange = useCallback(
     (changes) => setNodes((ns) => applyNodeChanges(changes, ns)),
@@ -47,8 +80,6 @@ const DnDFlow = () => {
       edges: edges,
     };
     setJson(JSON.stringify(flowchartData, null, 2))
-
-    console.log(JSON.stringify(flowchartData, null, 2));
   };
 
   const onConnect = useCallback(
@@ -97,13 +128,23 @@ const DnDFlow = () => {
 
   const nodeTypes = { textUpdater: TextUpdaterNode }
 
-  const onNodesDelete = (nodes) => {
-    console.log('Node clicked:', nodes);
-  }
 
-  const onNodeMouseEnter = (node) => {
-    console.log('Node mouse:', node);
-  }
+  const onNodeDoubleClick = (event, node) => {
+    console.log('test')
+    // const newName = prompt('Enter new name:', node.data.label);
+    // if (newName !== null && newName !== '') {
+    //   const updatedNode = {
+    //     ...node,
+    //     data: {
+    //       ...node.data,
+    //       label: newName,
+    //     },
+    //   };
+    //   // const updatedElements = (nodes, updatedNode);
+    //   setNodes(updatedNode);
+    // }
+  };
+
 
   return (
     <>
@@ -131,14 +172,24 @@ const DnDFlow = () => {
                 onInit={setReactFlowInstance}
                 onDrop={onDrop}
                 onDragOver={onDragOver}
-                onNodesDelete={onNodesDelete}
-                onNodeMouseEnter={onNodeMouseEnter}
                 fitView
+                onNodeDoubleClick={onNodeDoubleClick}
               >
-                <Controls />
+                <div className="updatenode__controls">
+                  <label>label:</label>
+                  <input
+                    value={nodeName}
+                    onChange={(evt) => setNodeName(evt.target.value)}
+                  />
+
+
+                </div>
+
+                {/* <Controls /> */}
               </ReactFlow>
             </div>
           )}
+
           {!showJson && (
             <pre>{json}</pre>
           )}
